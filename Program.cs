dc654c549efc797cdef7a5e260b59c3d6c0d24ce
Abdulkarim Ahmadov <148267382+abdulkarimahmadov@users.noreply.github.com>
2023-10-31 13:22:06 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static double KvadratiHesabla(double number)
+    {
+        double square = number * number;
+        return square;
+    }
+
+    static void Main()
+    {
+        double number = 10.0;
+        double result = KvadratiHesabla(number);
+
+        Console.WriteLine($"Kvadrat {number} is {result}");
+    }
+}
