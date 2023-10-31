13bb657553c963660cfebb1eb2b0234a18bd66de
Abdulkarim Ahmadov <148267382+abdulkarimahmadov@users.noreply.github.com>
2023-10-31 17:27:56 +0400

    Create task3.cs

---
+++ task3.cs
@@ -0,0 +1,10 @@
+public static List<int> GetNumbersUntilClosest(int number)
+{
+    List<int> numbers = new List<int>();
+    for (int i = 0; i <= number; i++)
+    {
+        numbers.Add(i);
+    }
+
+    return numbers;
+}
