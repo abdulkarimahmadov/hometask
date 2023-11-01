857d7a8ae3de88f7f57584a43c915ef140679317
Abdulkarim Ahmadov <148267382+abdulkarimahmadov@users.noreply.github.com>
2023-11-01 17:48:54 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void ResizeArray(ref int[] array, int newSize)
+    {
+        int[] newArray = new int[newSize];
+
+        for (int i = 0; i < newSize; i++)
+        {
+            if (i < array.Length)
+            {
+                newArray[i] = array[i];
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        array = newArray;
+    }
+
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Original array:");
+        foreach (var item in array)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+
+        int newSize = 12;
+        ResizeArray(ref array, newSize);
+
+        Console.WriteLine("Resized array:");
+        foreach (var item in array)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
